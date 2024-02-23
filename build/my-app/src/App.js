713f237dcf1c10ccf8e5cd3e8e9262800391b643
任佳颖 <renjiaying@renjiayingdeMacBook-Pro.local>
2024-02-23 11:28:38 -0330

    Add ski resort description

--- build/my-app/src/App.js
+++ build/my-app/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Header from './Header';
 import LiveInfo from './LiveInfo';
 import Footer from './Footer';
-
+import ContentPage from './ContentPage';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <ContentPage />
       <LiveInfo />
       <Footer />
     </div>
---
+++ build/my-app/src/ContentPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ContentPage.css';
+
+function ContentPage() {
+  return (
+    <div className="content-page">
+      <h1>xxxSki slope map</h1>
+      <h2>ready</h2>
+      <p>Explore xx ski resort</p >
+      <p>We have prepared interactive piste maps as well as 3D panoramic maps for you to prepare for your xxx skiing.
+      All ski lifts, pistes and mountain restaurants as well as ski area highlights are displayed in the ski area overview.
+      This way you can find all the information you need to make the most of your time at our ski resort. If you need to go
+      somewhere and don not know the route, you can click the button below and we will provide you with the fastest route.</p >
+    </div>
+  );
+}
+
+export default ContentPage;
